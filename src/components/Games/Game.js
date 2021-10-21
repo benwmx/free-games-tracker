03ff4874fd425fb4b09a30be3503d659fec894f7
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-21 13:38:38 +0100

    Render Games on screen.

--- src/components/Games/Game.js
+++ src/components/Games/Game.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
-function Game() {
-  const game = useSelector((state) => state.details);
+function Game(props) {
+  const { game } = props;
   return (
     <div className="gameContainer">
       <img className="image" src={game.thumbnail} alt="a game thumbnail" />
       <div className="details">
         <h2 className="title">{game.title}</h2>
         <p className="genre">{game.genre}</p>
-        <p className="releaseDate">{game.releaseDate}</p>
+        <p className="releaseDate">{game.release_date}</p>
         <p className="developer">{game.developer}</p>
       </div>
     </div>
   );
 }
+
+Game.propTypes = {
+  game: PropTypes.shape({
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    release_date: PropTypes.string.isRequired,
+    developer: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Game;
--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -1,23 +1,68 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { getListOfGames } from '../../redux/games';
+import Game from './Game';
 import Logo from '../../img/controller.png';
 
-const Games = () => (
-  <div className="container">
-    <div className="intro">
-      <img src={Logo} alt="game Controller" className="logo" />
-      <div className="introSearch">
-        <h1>Free Games Tracker</h1>
-        <p>Games from 1997 up to now</p>
-        <p>You can search by Title,Realease Date,Genre,Developer</p>
-        <form>
-          <input type="text" className="searchBar" placeholder="Search A Game" />
-        </form>
+const Games = () => {
+  const games = useSelector((state) => state.games);
+  const [searchResult, setSearchResult] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (games.length === 0) dispatch(getListOfGames());
+  }, []);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const result = games.filter((game) => {
+      const input = event.target.value.toLowerCase().trim();
+      const title = game.title.toLowerCase().trim();
+      const releaseDate = game.release_date.toLowerCase().trim();
+      const developer = game.developer.toLowerCase().trim();
+      const genre = game.genre.toLowerCase().trim();
+      return title.includes(input) || releaseDate.includes(input)
+            || genre.includes(input) || developer.includes(input);
+    });
+    setSearchResult(result);
+  };
+
+  const onGameClick = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <div className="container">
+      <div className="intro">
+        <img src={Logo} alt="game Controller" className="logo" />
+        <div className="introSearch">
+          <h1>Free Games Tracker</h1>
+          <p>Games from 1997 up to now</p>
+          <p>You can search by Title,Realease Date,Genre,Developer</p>
+          <form>
+            <input type="text" className="searchBar" placeholder="Search A Game" onChange={handleSearch} />
+          </form>
+        </div>
+      </div>
+      <div className="gamesContainer">
+        {
+          (searchResult === null)
+            ? games.map((game) => (
+              <NavLink key={game.id} to="/details" onClick={() => onGameClick(game.id)}>
+                <Game game={game} />
+              </NavLink>
+            ))
+            : searchResult.map((game) => (
+              <NavLink key={game.id} to="/details">
+                <Game game={game} />
+              </NavLink>
+            ))
+        }
       </div>
     </div>
-    <div className="gamesContainer">
-      <p>Empty</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Games;
