1f7670eddd614ef1667b3929f80e7d3626c421fa
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-21 15:24:27 +0100

    Add structure of minReqs table.

--- src/components/Details/Minrequirements.js
+++ src/components/Details/Minrequirements.js
@@ -2,8 +2,33 @@ import React from 'react';
 
 function Minrequirements() {
   return (
-    <div className="minRequirements">
-      <p>Empty</p>
+    <div className="minReqsContainer">
+      <p>Minimum System Requirements</p>
+      <table className="minReqsTable">
+        <thead />
+        <tbody>
+          <tr>
+            <td>OS</td>
+            <td>Bobo</td>
+          </tr>
+          <tr>
+            <td>processor</td>
+            <td>Bobo</td>
+          </tr>
+          <tr>
+            <td>Memory</td>
+            <td>Bobo</td>
+          </tr>
+          <tr>
+            <td>Graphics</td>
+            <td>Bobo</td>
+          </tr>
+          <tr>
+            <td>Storage</td>
+            <td>Bobo</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
