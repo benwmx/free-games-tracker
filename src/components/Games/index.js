b83ffcaaf8ecade38bdf366ec7a57f05fb6fadec
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-21 10:52:33 +0100

    Add logo & main page Structure

--- src/components/Games/index.js
+++ src/components/Games/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Logo from '../../img/controller.png';
 
 const Games = () => (
-  <div>
-    <p>EMpty Games</p>
+  <div className="container">
+    <div className="intro">
+      <img src={Logo} alt="game Controller" className="logo" />
+      <div className="introSearch">
+        <h1>Free Games Tracker</h1>
+        <p>Games from 1997 up to now</p>
+        <p>You can search by Title,Realease Date,Genre,Developer</p>
+        <form>
+          <input type="text" className="searchBar" placeholder="Search A Game" />
+        </form>
+      </div>
+    </div>
+    <div className="gamesContainer">
+      <p>Empty</p>
+    </div>
   </div>
 );
 
