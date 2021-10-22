c483db904b1fca8d7277db45463c7c942c58d5f6
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-22 16:28:14 +0100

    Add theme icons to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { BiLeftArrow } from 'react-icons/bi';
-import { BsSunFill, BsInfoCircleFill } from 'react-icons/bs';
+import { BsSunFill, BsInfoCircleFill, BsMoonFill } from 'react-icons/bs';
 import { clearDetails, unclearDetails } from '../redux/details';
+import { toggleTheme } from '../redux/theme';
 import Styles from './header.module.scss';
 
 const Header = () => {
   const details = useSelector((state) => state.details);
+  const theme = useSelector((state) => state.theme);
   const dispatch = useDispatch();
 
   const onClickArrow = () => {
@@ -18,13 +20,21 @@ const Header = () => {
     dispatch(unclearDetails());
   };
 
+  const onClickTheme = () => {
+    dispatch(toggleTheme());
+  };
+
   return (
     <header className={Styles.header}>
       <NavLink to="/" onClick={onClickArrow}>
         <BiLeftArrow className={Styles.headerBtns} style={(details.empty) ? { display: 'none' } : {}} />
       </NavLink>
       <div className={Styles.infoAndTheme}>
-        <BsSunFill className={Styles.headerBtns} />
+        {
+          (theme.darkTheme)
+            ? <BsSunFill className={Styles.headerBtns} onClick={onClickTheme} />
+            : <BsMoonFill className={Styles.headerBtns} onClick={onClickTheme} />
+        }
         <NavLink to="/about">
           <BsInfoCircleFill className={Styles.headerBtns} onClick={onClickInfo} />
         </NavLink>
