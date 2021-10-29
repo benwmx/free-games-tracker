c840ba97ef559f33c9a0c55d95acfd1049dbcf47
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-29 11:53:24 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
-import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(
@@ -14,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
